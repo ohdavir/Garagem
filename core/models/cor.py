8d92da3bccd7180a378d8025303a89670e90c54e
ohdavir <davi.rosa.info@gmail.com>
2025-04-28 16:21:36 -0300

    feat: Criação API Cor

---
+++ core/models/cor.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Cor(models.Model):
+    nome = models.CharField(max_length=40)
+    id = models.AutoField(primary_key=True)
+    
+    def __str__(self):
+        return self.nome    
+        return self.id
